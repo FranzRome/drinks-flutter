5e4d8763d8a30264ae749e126deaf2629c66e506
Franz <francesco.romeo-work@libero.it>
2024-02-12 11:27:35 +0100

    Primary Color Update

--- lib/globals/my_theme.dart
+++ lib/globals/my_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class MyTheme{
   static const Color chalkWhite = Color(0xFFFBFFFF);
   static const Color background = Color(0xFFE0E0E0);
-  static const Color primary = Color(0xFFBB6000);
+  static const Color primary = Color(0xFFCC6000);
   static const Color lightText = Color(0xFF404040);
 }
